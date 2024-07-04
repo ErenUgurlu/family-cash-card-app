455dc79f5f7ec3c197a1822bbeaca7cf79f2e638
tr-eren.ugurlu <erentrugurlu@gmail.com>
2024-07-04 17:33:46 +0300

    Lab: Returning a list with GET

--- src/main/java/com/spring/learn/cash/card/CashCardController.java
+++ src/main/java/com/spring/learn/cash/card/CashCardController.java
@@ -1,31 +1,66 @@
 package com.spring.learn.cash.card;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.*;
 
 @RestController
 @RequestMapping("/cashcards")
-public class CashCardController {
+class CashCardController {
+    private final CashCardRepository cashCardRepository;
 
-	private final CashCardRepository cashCardRepository;
+    private CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
 
-	private CashCardController(CashCardRepository cashCardRepository) {
-		this.cashCardRepository = cashCardRepository;
-	}
+    @GetMapping("/{requestedId}")
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+        if (cashCardOptional.isPresent()) {
+            return ResponseEntity.ok(cashCardOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
-	@GetMapping("/{requestedId}")
-	private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-	    Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
-	    if (cashCardOptional.isPresent()) {
-	        return ResponseEntity.ok(cashCardOptional.get());
-	    } else {
-	        return ResponseEntity.notFound().build();
-	    }
-	}
+    @PostMapping
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+        URI locationOfNewCashCard = ucb
+                .path("cashcards/{id}")
+                .buildAndExpand(savedCashCard.ID())
+                .toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
+    }
 
+    @GetMapping
+    private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(
+        		PageRequest.of(
+        		        pageable.getPageNumber(),
+        		        pageable.getPageSize(),
+        		        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+        		));
+        return ResponseEntity.ok(page.getContent());
+    }
+    
 }
--- src/test/java/com/spring/learn/cash/card/CashCardJsonTest.java
+++ src/test/java/com/spring/learn/cash/card/CashCardJsonTest.java
@@ -1,5 +1,6 @@
 package com.spring.learn.cash.card;
 
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,36 +18,46 @@ import static org.assertj.core.api.Assertions.assertThat;
 @JsonTest
 class CashCardJsonTest {
 
+    @Autowired
     private JacksonTester<CashCard> json;
+
+    @Autowired
+    private JacksonTester<CashCard[]> jsonList;
+
+    private CashCard[] cashCards;
 
     @BeforeEach
     void setUp() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        JacksonTester.initFields(this, objectMapper);
+        cashCards = Arrays.array(
+                new CashCard(99L, 123.45),
+                new CashCard(100L, 100.00),
+                new CashCard(101L, 150.00));
     }
+
     @Test
     void cashCardSerializationTest() throws IOException {
-        CashCard cashCard = new CashCard(99L, 123.45);
-        assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
-        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.ID");
-        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.ID")
+        CashCard cashCard = cashCards[0];
+        assertThat(json.write(cashCard)).isStrictlyEqualToJson("single.json");
+        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id")
                 .isEqualTo(99);
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount")
-             .isEqualTo(123.45);
+                .isEqualTo(123.45);
     }
-    
+
     @Test
     void cashCardDeserializationTest() throws IOException {
-       String expected = """
-               {
-                   "ID":99,
-                   "amount":123.45
-               }
-               """;
-       assertThat(json.parse(expected))
-               .isEqualTo(new CashCard(99L, 123.45));
-       assertThat(json.parseObject(expected).ID()).isEqualTo(99);
-       assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
+        String expected = """
+                {
+                    "id": 99,
+                    "amount": 123.45
+                }
+                """;
+        assertThat(json.parse(expected))
+                .isEqualTo(new CashCard(99L, 123.45));
+        assertThat(json.parseObject(expected).ID()).isEqualTo(99);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
     }
 }
+
